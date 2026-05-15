8fc21b2d022dd8f5c25b323a06cf529fcb6c9c1b
subhajitdeo <subhajitdeoghoria5@gmail.com>
2026-05-15 20:48:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,53 @@
-async function loadData(){
+<div class="details">
 
-    const response = await fetch("data/results.json");
+    <div class="chart-container">
 
-    const result = await response.json();
+        <div class="line"></div>
 
-    const stocks = result.data;
+        <div class="dot price"
+            style="left:95%">
+        </div>
 
-    const stockList = document.getElementById("stockList");
+        <div class="dot ema20"
+            style="left:${(stock.ema20/maxValue)*100}%">
+        </div>
 
-    stockList.innerHTML = "";
+        <div class="dot ema50"
+            style="left:${(stock.ema50/maxValue)*100}%">
+        </div>
 
-    stocks.forEach(stock => {
+        <div class="dot ema100"
+            style="left:${(stock.ema100/maxValue)*100}%">
+        </div>
 
-        const maxValue = Math.max(
-            stock.price,
-            stock.ema20,
-            stock.ema50,
-            stock.ema100,
-            stock.ema200
-        );
+        <div class="dot ema200"
+            style="left:${(stock.ema200/maxValue)*100}%">
+        </div>
 
-        const card = document.createElement("div");
+    </div>
 
-        card.className = "stock-card";
+    <div class="legend">
 
-        card.innerHTML = `
+        <div>
+            <span class="legend-dot ema20"></span>
+            EMA20 : ${stock.ema20}
+        </div>
 
-            <div class="stock-header">
+        <div>
+            <span class="legend-dot ema50"></span>
+            EMA50 : ${stock.ema50}
+        </div>
 
-                <div>
-                    <h2>${stock.symbol}</h2>
-                    <h3>₹${stock.price}</h3>
-                </div>
+        <div>
+            <span class="legend-dot ema100"></span>
+            EMA100 : ${stock.ema100}
+        </div>
 
-                <div>
-                    Score: ${stock.score}
-                </div>
+        <div>
+            <span class="legend-dot ema200"></span>
+            EMA200 : ${stock.ema200}
+        </div>
 
-            </div>
+    </div>
 
-            <div class="details">
-
-                <p>EMA20 : ${stock.ema20}</p>
-
-                <div class="bar">
-                    <div class="fill ema20"
-                    style="width:${(stock.ema20/maxValue)*100}%">
-                    </div>
-                </div>
-
-                <p>EMA50 : ${stock.ema50}</p>
-
-                <div class="bar">
-                    <div class="fill ema50"
-                    style="width:${(stock.ema50/maxValue)*100}%">
-                    </div>
-                </div>
-
-                <p>EMA100 : ${stock.ema100}</p>
-
-                <div class="bar">
-                    <div class="fill ema100"
-                    style="width:${(stock.ema100/maxValue)*100}%">
-                    </div>
-                </div>
-
-                <p>EMA200 : ${stock.ema200}</p>
-
-                <div class="bar">
-                    <div class="fill ema200"
-                    style="width:${(stock.ema200/maxValue)*100}%">
-                    </div>
-                </div>
-
-            </div>
-        `;
-
-        const header = card.querySelector(".stock-header");
-        const details = card.querySelector(".details");
-
-        header.addEventListener("click", () => {
-
-            if(details.style.display === "block"){
-                details.style.display = "none";
-            }
-            else{
-                details.style.display = "block";
-            }
-
-        });
-
-        stockList.appendChild(card);
-
-    });
-
-}
-
-loadData();
+</div>
